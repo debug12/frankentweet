9ebd236c79628bf90eb8bc2c5665c37860c97644
debug12 <debug12@gmail.com>
2014-05-24 23:04:09 -0400

    adding express 4.x stuff

--- frankentweet/routes/index.js
+++ frankentweet/routes/index.js
@@ -2,8 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.use(function(req, res, next){
+	console.log(req.method, req.url, req.host);
+	next();
+})
+
+router.param('name', function(req, res, next, name){
+	console.log('doing name validation on ' + req.params.name);
+	req.name = name;
+	next();
+})
+
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/hello/:name', function(req, res){
+	res.send('hello ' + req.name + '!');
+})
+
+router.post('/', function(req, res){
+	res.redirect('/');
+})
 
 module.exports = router;
